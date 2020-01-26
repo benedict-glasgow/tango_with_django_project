8cef6dee806e666b4bf4f3435b3104d092eb51f3
Benedict Anslow <2382378a@student.gla.ac.uk>
2020-01-26 21:41:01 +0000

    Completed 4.3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,6 +18,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Template paths
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+# Static directory
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+# Media directory
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -65,10 +71,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
+
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
@@ -121,3 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Media
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
